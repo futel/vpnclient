42bc3c93f0ab98e1a1d9a7cc929e82ca8f9c9aec
Russell Senior <russell@personaltelco.net>
2022-07-29 18:17:38 -0700

    make firewall update more surgical (#14)
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- lede/update-firewall.sh
+++ lede/update-firewall.sh
@@ -3,147 +3,22 @@
 # one-off script to update openvpn config of running openwrt/lede router
 # to match commit f77f54d4b1d02e89ac4af25620bc382386f0dd96
 
-rm /etc/config/firewall
+# look for config stanza handle associated with firewall zone
+cfg=$(uci -X show firewall | grep 'network' | grep 'vpn' | cut -d. -f2)
 
-cat >/etc/config/firewall <<EOF
+# change vpn zone forward policy to ACCEPT
+uci set firewall.$cfg.forward=ACCEPT
 
-config defaults
-	option syn_flood '1'
-	option input 'ACCEPT'
-	option output 'ACCEPT'
-	option forward 'REJECT'
+# look for config stanza handle associated with firewall forwarding rules
+cfg=$(uci -X show firewall | grep src | grep vpn | cut -d. -f2)
 
-config zone
-	option name 'lan'
-	list network 'lan'
-	option input 'ACCEPT'
-	option output 'ACCEPT'
-	option forward 'ACCEPT'
+# if it doesn't exist, create one to allow vpn-to-lan traffic
+if [ -z "$cfg" ] ; then
+	uci add firewall forwarding
+	uci set firewall.@forwarding[-1].dest=lan
+	uci set firewall.@forwarding[-1].src=vpn
+fi
 
-config zone
-	option name 'wan'
-	list network 'wan'
-	list network 'wan6'
-	option input 'REJECT'
-	option output 'ACCEPT'
-	option forward 'REJECT'
-	option masq '1'
-	option mtu_fix '1'
-
-config forwarding
-	option src 'lan'
-	option dest 'wan'
-
-config rule
-	option name 'Allow-DHCP-Renew'
-	option src 'wan'
-	option proto 'udp'
-	option dest_port '68'
-	option target 'ACCEPT'
-	option family 'ipv4'
-
-config rule
-	option name 'Allow-Ping'
-	option src 'wan'
-	option proto 'icmp'
-	option icmp_type 'echo-request'
-	option family 'ipv4'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-IGMP'
-	option src 'wan'
-	option proto 'igmp'
-	option family 'ipv4'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-DHCPv6'
-	option src 'wan'
-	option proto 'udp'
-	option dest_port '546'
-	option family 'ipv6'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-MLD'
-	option src 'wan'
-	option proto 'icmp'
-	option src_ip 'fe80::/10'
-	list icmp_type '130/0'
-	list icmp_type '131/0'
-	list icmp_type '132/0'
-	list icmp_type '143/0'
-	option family 'ipv6'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-ICMPv6-Input'
-	option src 'wan'
-	option proto 'icmp'
-	list icmp_type 'echo-request'
-	list icmp_type 'echo-reply'
-	list icmp_type 'destination-unreachable'
-	list icmp_type 'packet-too-big'
-	list icmp_type 'time-exceeded'
-	list icmp_type 'bad-header'
-	list icmp_type 'unknown-header-type'
-	list icmp_type 'router-solicitation'
-	list icmp_type 'neighbour-solicitation'
-	list icmp_type 'router-advertisement'
-	list icmp_type 'neighbour-advertisement'
-	option limit '1000/sec'
-	option family 'ipv6'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-ICMPv6-Forward'
-	option src 'wan'
-	option dest '*'
-	option proto 'icmp'
-	list icmp_type 'echo-request'
-	list icmp_type 'echo-reply'
-	list icmp_type 'destination-unreachable'
-	list icmp_type 'packet-too-big'
-	list icmp_type 'time-exceeded'
-	list icmp_type 'bad-header'
-	list icmp_type 'unknown-header-type'
-	option limit '1000/sec'
-	option family 'ipv6'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-IPSec-ESP'
-	option src 'wan'
-	option dest 'lan'
-	option proto 'esp'
-	option target 'ACCEPT'
-
-config rule
-	option name 'Allow-ISAKMP'
-	option src 'wan'
-	option dest 'lan'
-	option dest_port '500'
-	option proto 'udp'
-	option target 'ACCEPT'
-
-config zone
-	option input 'ACCEPT'
-	option forward 'ACCEPT'
-	option output 'ACCEPT'
-	option name 'vpn'
-	option masq '1'
-	option mtu_fix '1'
-	option network 'vpn'
-
-config forwarding
-	option dest 'vpn'
-	option src 'lan'
-
-config forwarding
-	option dest 'lan'
-	option src 'vpn'
-
-EOF
+uci commit firewall
 
 /etc/init.d/firewall restart
